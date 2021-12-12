b386dfe02499c8c3dca3a1e38cbe49f23bab7429
Tyler Ryan <tlrrn5@gmail.com>
2021-12-12 16:23:24 -0600

    Fixing install script

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 home=$HOME
 pyinstaller_compat_python=python3.6
 install (){
-    cd ~/pipe-dev
+    cd ~/pipe_dev
     python3 -m pip install virtualenv --user || exit 1;
     # Pyinstaller doesn't work for python3.10
     python3 -m virtualenv ~/.python_environs/default -p /usr/bin/$pyinstaller_compat_python || exit 1;
@@ -12,7 +12,7 @@ install (){
     echo "user_env_location = $home/.python_environs/" > ~/.config/pipe/config.ini || exit 1 ;
     source ~/.python_environs/default/bin/activate || exit 1 ;
     pip install -e . || exit 1 ;
-    pipe-dev app install pipe  || exit 1 ;
+    pipe_dev app install pipe  || exit 1 ;
     echo
     echo
     pipe || exit 1 ;
